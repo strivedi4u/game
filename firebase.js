df686d1e0e613ca170d5878132f9e723492be34f
Shashank <81739538+trivedi2u@users.noreply.github.com>
2022-02-10 10:40:54 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBJWGNjgW_fTYhtWRxKYDaF_U56T1TzfB4",
+ authDomain: "gamelogin-10d4e.firebaseapp.com",
+ projectId: "gamelogin-10d4e",
+ storageBucket: "gamelogin-10d4e.appspot.com",
+ messagingSenderId: "231051263785",
+ appId: "1:231051263785:web:1b30bee9485fa7b87e8138"
+};
+ // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
